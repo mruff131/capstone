e7afa5ea7bd5c2f8508db52bf5946333149315b1
Mariah <ruffmariah2@gmail.com>
2023-10-30 22:28:07 -0500

    changed hello world

--- ChallengingCodeWars/Codewar.py
+++ ChallengingCodeWars/Codewar.py
@@ -1,8 +1,13 @@
-# take the numbers 3 & 5 and multiple them by range 1 through ...
-# store the number in a list , if the number isn't already in the list
-# tell program to stop once the multiple is greater than the number given
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
 
+Additionally, if the number is negative, return 0.
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+"""
 
 def solution(number):
     total = 0
